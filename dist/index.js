94a87f13b5fc2c29ff1a7b5732bb7b7daf1aeb08
SohamDesai1 <sohamdesai123@gmail.com>
2023-01-04 12:55:02 +0530

    added config,launch json and debugged

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,7 @@
 "use strict";
 let age = 25;
+if (age < 40) {
+    age = +40;
+}
+console.log(age);
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-let age : number = 25;
+let age: number = 25;
+if (age < 40) {
+  age = +40;
+}
+console.log(age);
