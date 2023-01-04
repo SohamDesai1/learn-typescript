f1609f484fc04ba8567b7913cfdc187d40cd839d
SohamDesai1 <sohamdesai123@gmail.com>
2023-01-04 13:26:50 +0530

    enums

---
+++ dist/enum.js
@@ -0,0 +1,14 @@
+"use strict";
+// var Division;
+// (function (Division) {
+//     Division[Division["Primnary"] = 0] = "Primnary";
+//     Division[Division["Middle"] = 1] = "Middle";
+//     Division[Division["High"] = 2] = "High";
+//     Division[Division["College"] = 3] = "College";
+//     Division[Division["University"] = 4] = "University";
+// })(Division || (Division = {}));
+// let myDivision = Division.Middle;
+// console.log(myDivision);
+
+let myDivision = 1;
+console.log(myDivision);
---
+++ src/enum.ts
@@ -0,0 +1,7 @@
+// enum Division {Primnary, Middle, High, College, University}
+// let myDivision = Division.Middle;
+// console.log(myDivision); This code generates a longer js file
+
+const enum Division {Primnary, Middle, High, College, University}
+let myDivision = Division.Middle;
+console.log(myDivision); 
