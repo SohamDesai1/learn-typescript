f01a8e8f9bda4b0797face188c091f3afe38132d
SohamDesai1 <sohamdesai123@gmail.com>
2023-01-04 13:13:16 +0530

    tuples

--- dist/arrays.js
+++ dist/arrays.js
@@ -2,3 +2,4 @@
 let arr = [1, 2, 3, 4, 5];
 let arr2 = [];
 arr.forEach(n => console.log(n.valueOf()));
+let tuple = [1, "2", true];
--- src/arrays.ts
+++ src/arrays.ts
@@ -2,3 +2,5 @@ let arr : number[] = [1, 2, 3, 4, 5];
 let arr2 =[]; // type any[]
 
 arr.forEach(n=>console.log(n.valueOf()));
+
+let tuple : [number, string, boolean] = [1, "2", true];
